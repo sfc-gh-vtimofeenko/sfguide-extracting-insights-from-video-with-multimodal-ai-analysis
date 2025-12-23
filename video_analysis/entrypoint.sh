0fa377723fdd54733df66c5598e9c378021c2090
Vladimir Timofeenko <vladimir.timofeenko@snowflake.com>
2025-12-23 14:21:41 -0800

    pin qwen-vl-utils to a version matching the repo age
    Reasoning also added in the code comment

--- video_analysis/entrypoint.sh
+++ video_analysis/entrypoint.sh
@@ -4,7 +4,9 @@
 python3 -m pip install --upgrade pip
 pip install git+https://github.com/huggingface/transformers@f3f6c86582611976e72be054675e2bf0abb5f775
 pip install accelerate
-pip install qwen-vl-utils
+# Some code in this repo relies on a specific version of qwen
+# Without this change there will be an error "TypeError: Qwen2_5_VLProcessingInfo.get_hf_processor() got an unexpected keyword argument 'do_sample_frames'"
+pip install qwen-vl-utils==0.0.10
 pip install click
 pip install snowflake-connector-python
 
